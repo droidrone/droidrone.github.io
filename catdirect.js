74d164dedb94d88e4423a3838ecc5e9873024682
droidrone <asaefull@outlook.com>
2025-07-21 12:28:47 +0700

    reupload part3

--- catdirect.js
+++ catdirect.js
@@ -1,1 +1,14 @@
-www.google.com
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    window.open('https://mixtureanticipationsuede.com/vjrjhids2?key=da38f77b19581048239a5db27b36e9d9', '_blank');
+	});
+</script>
